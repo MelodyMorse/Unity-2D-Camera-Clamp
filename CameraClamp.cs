79fdc35cf6ce0e25f3c0a9a06663c85f21f34b62
Melody Morse <14865089+MelodyMorse@users.noreply.github.com>
2020-10-30 23:21:57 -0400

    first main commit

---
+++ CameraClamp.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+    [SerializeField] [HideInInspector] private float _xMin;
+    public float xMin
+    {
+        get { return _xMin; }
+        set
+        {
+            if (value > transform.position.x - horizontalExtents) return;
+            else _xMin = value;
+        }
+    }
+
+    [SerializeField] [HideInInspector] private float _xMax;
+    public float xMax
+    {
+        get { return _xMax; }
+        set
+        {
+            if (value < transform.position.x + horizontalExtents) return;
+            else _xMax = value;
+        }
+    }
+
+    [SerializeField] [HideInInspector] private float _yMin;
+    public float yMin
+    { 
+        get { return _yMin; }
+        set
+        {
+            if (value > transform.position.y - verticalExtents) return;
+            else _yMin = value;
+        }
+    }
+
+    [SerializeField] [HideInInspector] private float _yMax;
+    public float yMax
+    {
+        get { return _yMax; }
+        set
+        {
+            if (value < transform.position.y + verticalExtents) return;
+            else _yMax = value;
+        }
+    }
+
+    [SerializeField] [HideInInspector] private bool initialized;
+    private float verticalExtents;
+    private float horizontalExtents;
+    private Camera cam;
+
+    private void OnValidate()
+    {
+        cam = GetComponent<Camera>();
+        if (!initialized)
+        {
+            Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        CalculateExtents();
+        
+    }
+    void LateUpdate()
+    {
+        Clamp();
+    }
+    private void OnDrawGizmos()
+    {
+        Vector2 topLeft = new Vector2(xMin, yMax);
+        Vector2 topRight = new Vector2(xMax, yMax);
+        Vector2 bottomLeft = new Vector2(xMin, yMin);
+        Vector2 bottomRight = new Vector2(xMax, yMin);
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topLeft, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, bottomRight);
+        Gizmos.DrawLine(bottomRight, topRight);
+    }
+    //This method is called once in OnValidate once when this monobehavior is added to a gameobject
+    private void Initialize()
+    {
+        CalculateExtents();
+        //initialize values
+        xMin = transform.position.x - horizontalExtents;
+        xMax = transform.position.x + horizontalExtents;
+        yMin = transform.position.y - verticalExtents;
+        yMax = transform.position.y + verticalExtents;
+        
+        initialized = true;
+    }
+    private void CalculateExtents()
+    {
+        verticalExtents = cam.orthographicSize;
+        horizontalExtents = cam.orthographicSize * cam.aspect;
+    }
+    private void Clamp()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin + horizontalExtents, xMax - horizontalExtents),
+                            Mathf.Clamp(transform.position.y, yMin + verticalExtents, yMax - verticalExtents),
+                            transform.position.z);
+    }
+}
+
---
+++ Editor/CameraClampEditor.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraClamp))]
+public class CameraClampEditor: Editor
+{
+    CameraClamp clamp;
+    private void OnEnable()
+    {
+        clamp = target as CameraClamp; 
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        clamp.xMin = EditorGUILayout.FloatField("X Min",clamp.xMin);
+        clamp.xMax = EditorGUILayout.FloatField("X Max", clamp.xMax);
+        clamp.yMin = EditorGUILayout.FloatField("Y Min", clamp.yMin);
+        clamp.yMax = EditorGUILayout.FloatField("Y Max", clamp.yMax);
+    }   
+}
