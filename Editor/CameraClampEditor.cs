b4b8fed261740b37d44f99ddaa9df58bf4410fab
Melody Morse <14865089+MelodyMorse@users.noreply.github.com>
2020-11-04 16:07:06 -0500

    Handles

--- Editor/CameraClampEditor.cs
+++ Editor/CameraClampEditor.cs
@@ -1,11 +1,11 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(CameraClamp))]
 public class CameraClampEditor: Editor
 {
     CameraClamp clamp;
+    float snap = .01f;
     private void OnEnable()
     {
         clamp = target as CameraClamp; 
@@ -17,5 +17,14 @@ public class CameraClampEditor: Editor
         clamp.xMax = EditorGUILayout.FloatField("X Max", clamp.xMax);
         clamp.yMin = EditorGUILayout.FloatField("Y Min", clamp.yMin);
         clamp.yMax = EditorGUILayout.FloatField("Y Max", clamp.yMax);
-    }   
+    }
+    private void OnSceneGUI()
+    {
+        Vector3 pos = clamp.transform.position;
+        
+        clamp.xMax = Handles.FreeMoveHandle(new Vector3(clamp.xMax, pos.y, pos.z), clamp.transform.rotation, .2f, new Vector3(snap, snap), Handles.CubeHandleCap).x;
+        clamp.xMin = Handles.FreeMoveHandle(new Vector3(clamp.xMin, pos.y, pos.z), clamp.transform.rotation,.2f, new Vector3(snap, snap), Handles.CubeHandleCap).x;
+        clamp.yMax = Handles.FreeMoveHandle(new Vector3(pos.x, clamp.yMax, pos.z), clamp.transform.rotation, .2f, new Vector3(snap, snap), Handles.CubeHandleCap).y;
+        clamp.yMin = Handles.FreeMoveHandle(new Vector3(pos.x, clamp.yMin, pos.z), clamp.transform.rotation, .2f, new Vector3(snap, snap), Handles.CubeHandleCap).y;
+    }
 }
